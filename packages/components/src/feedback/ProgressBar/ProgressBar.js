731cd60bb75bdfa1557e442ffac6ac29c115cfb4
Alberto Martinez <amperez@dminc.com>
2021-11-12 15:23:46 +0000

    Merged in feature/ABMD-657-wrap-progress-bar (pull request #133)
    Add progress bar
    
    * Add progress bar
    
    Signed-off-by: aldevgurus <alberto.martinez@devgurus.io>
    
    * Parametrize variant and change sizes
    
    * Add default width by argument
    
    * Add prettier config so it does not conflict with eslint
    
    * Add react dependency
    
    * Change height according to new designs
    
    
    Approved-by: Fernando Lucero
    Approved-by: Anderson Miranda
    Approved-by: JI Gumpert

---
+++ packages/components/src/feedback/ProgressBar/ProgressBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ProgressBar as BSPProgressBar } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const ProgressBar = ({
+  percentage, variant, width, ...rest
+}) => {
+  return (
+    <BSPProgressBar
+      now={percentage}
+      variant={variant}
+      {...rest}
+      style={{ width }}
+    />
+  );
+};
+
+ProgressBar.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  variant: PropTypes.string,
+  width: PropTypes.string,
+};
+
+ProgressBar.defaultProps = {
+  variant: 'dark',
+  width: '100%',
+};
+export default ProgressBar;
--- packages/components/src/feedback/index.js
+++ packages/components/src/feedback/index.js
@@ -1,2 +1,3 @@
 export * from './Modal';
 export * from './Snackbar';
+export * from './ProgressBar';
