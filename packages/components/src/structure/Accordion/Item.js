0a0e261daa424364fe471a03ef7cbff25cf4812e
Anderson Miranda <amiranda@dminc.com>
2021-11-28 11:56:14 +0100

    fixes: Accordion Item classNames cssStyles props

--- packages/components/src/structure/Accordion/Item.js
+++ packages/components/src/structure/Accordion/Item.js
@@ -2,16 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AccordionItem from 'react-bootstrap/AccordionItem';
 
-const Item = ({ cssInternalPrefix, children, eventKey }) => {
+const Item = ({
+  className,
+  cssStyles,
+  cssInternalPrefix,
+  children,
+  eventKey,
+}) => {
   return (
-    <AccordionItem eventKey={eventKey} data-testid="mch-accordion-item" bsPrefix={cssInternalPrefix}>
-        {children}
+    <AccordionItem
+      eventKey={eventKey}
+      data-testid="mch-accordion-item"
+      className={className}
+      styles={cssStyles}
+      bsPrefix={cssInternalPrefix}
+    >
+      {children}
     </AccordionItem>
   );
 };
 Item.propTypes = {
   eventKey: PropTypes.string.isRequired,
   cssInternalPrefix: PropTypes.string,
+  className: PropTypes.string,
+  cssStyles: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 Item.defaultProps = {
