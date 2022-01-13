da743cbc640b2d36ab1ec370f9a62a211f8167dc
bruno cantero <bcantero@dminc.com>
2022-01-13 10:56:14 +0000

    fix (Icons): added icon unit test

--- packages/components/src/utils/Icon/Icon.spec.js
+++ packages/components/src/utils/Icon/Icon.spec.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Icon from './Icon';
+import { render, screen, cleanup } from '@testing-library/react';
+import Icon, { IconList } from './Icon';
 
 const testElementId = 'mch-icon';
 
 describe('Icon component', () => {
-  test('render button', () => {
+  test('render one icon', () => {
     render(<Icon name="delete" />);
 
     const el = screen.getByTestId(testElementId);
 
     expect(el).toBeDefined();
+  });
+});
+
+describe('Tests for each Icon component', () => {
+  afterEach(cleanup);
+
+  test.each(IconList)('Icon $icon', (icon) => {
+    const element = render(<Icon name={icon} />);
+
+    const iconRendered = element.getByTestId(testElementId);
+
+    expect(iconRendered).toBeInTheDocument();
   });
 });
