67f4874e30778203b32bb3f87b46b0a379ea2024
pmagalhaes@dminc.com <pmagalhaes@dminc.com>
2022-02-18 12:02:14 +0000

    feat: special room card

--- packages/components/src/structure/cards/SpecialRoomCard/SpecialRoomCard.js
+++ packages/components/src/structure/cards/SpecialRoomCard/SpecialRoomCard.js
@@ -1,71 +1,216 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card as BSPCard } from 'react-bootstrap';
-import classnames from 'classnames';
-import { truncateText } from '../../../helpers/truncateText';
-
-const truncateValues = {
-  title: 60,
-  description: 180,
-};
-
-const format = (dateInput) => new Date(dateInput).getFullYear();
+import classNames from 'classnames';
+import { ButtonIcon } from '../../../actions/ButtonIcon';
+import { Tag } from '../../../feedback/Tag';
+import { Icon } from '../../../utils/Icon';
+import { Checkbox } from '../../../inputs/Checkbox';
 
 const SpecialRoomCard = ({
-  size,
   title,
-  image,
-  room,
-  date,
-  name,
-  cssInternalPrefix,
-  cssStyles,
+  artistName,
+  sectorsData,
+  linkRenderer,
+  roomLink,
+  imageUrl,
+  openingSoon,
+  visited,
+  show,
+  unavailableToView,
+  selectMode,
+  onCollectionAdd,
 }) => {
-  const description = [name, format(date)].join(', ');
-  const truncated = {
-    title: truncateText(title, truncateValues.title),
-    description: truncateText(description, truncateValues.description),
+  const [isRoomSelected, setRoomSelected] = React.useState(selectMode?.checked);
+  const defaultState = !openingSoon?.active && !unavailableToView?.active && !selectMode?.active;
+
+  const ConditionalWrapper = ({ condition, link, children }) => {
+    if (condition && typeof linkRenderer === 'function') {
+      return linkRenderer(link, children);
+    }
+    return children;
+  };
+
+  const onSelectRoom = (evt) => {
+    setRoomSelected(evt?.target?.checked);
+    selectMode.onChange();
   };
 
   return (
     <BSPCard
       data-testid="mch-specialroom-card"
-      bsPrefix={cssInternalPrefix}
-      style={cssStyles}
-      className={classnames('specialroom-card', `size-${size}`)}
+      className={classNames('card specialroom-card', {
+        'default-state': defaultState,
+        'opening-state': openingSoon?.active,
+        'unavailable-state': unavailableToView?.active,
+        'select-state': selectMode?.active,
+        'selected-state': selectMode?.active && isRoomSelected,
+      })}
     >
-      <div className="gradient" />
-      <img src={image} alt="picture" />
-      <BSPCard.Body>
-        <BSPCard.Text className="room">{room}</BSPCard.Text>
-        <BSPCard.Title>
-          {truncated.title.text}
-          {truncated.title.state && <span title={title}>...</span>}
-        </BSPCard.Title>
-        <BSPCard.Text className="date">
-          {truncated.description.text}
-          {truncated.description.state && <span title={description}>...</span>}
-        </BSPCard.Text>
-      </BSPCard.Body>
+      {/* Image Frame */}
+      <div className="image-frame">
+        {/* tag / unavailable to view */}
+        {unavailableToView?.active && (
+          <Tag
+            label={unavailableToView.label}
+            onClick={unavailableToView.onClick}
+            className="tag-unavailableToView"
+            icon="Info"
+            iconAlign="left"
+          />
+        )}
+
+        {/* tag / openingSoon */}
+        {openingSoon?.active && (
+          <Tag
+            label={openingSoon.label}
+            onClick={openingSoon.onClick}
+            className="tag-opening"
+          />
+        )}
+
+        {/* tag / visited */}
+        {visited?.active && (
+          <Tag
+            label={visited.label}
+            onClick={visited.onClick}
+            className="tag-visited"
+          />
+        )}
+
+        {/* tag / sector */}
+        {sectorsData?.length > 0 && (
+          <div className="sectors">
+            {sectorsData?.map((i) => (
+              <Tag
+                key={i.id}
+                label={i.name}
+                sector={i.name.toLowerCase()}
+                type="sectors"
+                theme="dark"
+                onClick={i.onClick}
+              />
+            ))}
+          </div>
+        )}
+
+        {/* tag / show name */}
+        {show?.active && (
+          <div className="showname">
+            <Tag
+              type="label"
+              variant="secondary"
+              label={show.label}
+              onClick={show.onClick}
+            />
+          </div>
+        )}
+
+        {/* icon / add to collection */}
+        {!selectMode?.active && (
+          <ButtonIcon
+            icon="collections-add"
+            onClick={onCollectionAdd}
+            variant="fill"
+            theme="dark"
+          />
+        )}
+
+        {/* Texts (room title and artist name) */}
+        <div className="text-frame">
+          {title && (
+            <ConditionalWrapper condition={defaultState} link={roomLink}>
+              <h3>{title}</h3>
+            </ConditionalWrapper>
+          )}
+          {artistName && (
+            <ConditionalWrapper condition={defaultState} link={roomLink}>
+              <h5 className="truncate" title={artistName}>
+                {artistName}
+              </h5>
+            </ConditionalWrapper>
+          )}
+        </div>
+
+        {/* select mode */}
+        {selectMode?.active && (
+          <Checkbox
+            checked={isRoomSelected}
+            disabled={selectMode.disabled}
+            onChange={onSelectRoom}
+            className="checkbox-select"
+          />
+        )}
+
+        {/* overlay / unavailable */}
+        {unavailableToView?.active && (
+          <div className="overlay-eye">
+            <Icon name="eye-hide" />
+          </div>
+        )}
+
+        {/* overlay / blur, fill, gradient */}
+        <ConditionalWrapper condition={defaultState} link={roomLink}>
+          <div className="overlay-gradient" />
+          <div className="overlay-fill dark" />
+          <div
+            className={classNames('overlay-blur', {
+              dark: openingSoon?.active,
+              light: unavailableToView?.active,
+            })}
+          />
+          <img src={imageUrl} title={title} alt={title} />
+        </ConditionalWrapper>
+      </div>
     </BSPCard>
   );
 };
 
 SpecialRoomCard.propTypes = {
-  size: PropTypes.oneOf(['s', 'l']),
   title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  room: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  cssInternalPrefix: PropTypes.string,
-  cssStyles: PropTypes.string,
+  linkRenderer: PropTypes.func.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  artistName: PropTypes.string,
+  roomLink: PropTypes.string,
+  sectorsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      onClick: PropTypes.func,
+    }),
+  ),
+  openingSoon: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+  }),
+  visited: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+  }),
+  show: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+  }),
+  unavailableToView: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+  }),
+  selectMode: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    checked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+  }),
+  onCollectionAdd: PropTypes.func,
 };
 
 SpecialRoomCard.defaultProps = {
-  size: 's',
-  cssInternalPrefix: 'card',
-  cssStyles: null,
+  onCollectionAdd: () => {},
+  selected: false,
 };
 
 export default SpecialRoomCard;
