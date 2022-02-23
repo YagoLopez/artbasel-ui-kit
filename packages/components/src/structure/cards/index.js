91db88d3d25832a4d1a37b896ffaf7aab5b35ba0
pmagalhaes@dminc.com <pmagalhaes@dminc.com>
2022-02-23 08:39:25 +0000

    fix: fixing ovr room card feedbacks

--- packages/components/src/structure/cards/index.js
+++ packages/components/src/structure/cards/index.js
@@ -2,7 +2,6 @@ export * from './ArtistCard';
 export * from './ContentCard';
 export * from './EventCard';
 export * from './GalleryCard';
-export * from './GalleryOVRCard';
 export * from './SpecialRoomCard';
 export * from './StoryCard';
 export * from './TeaserCard';
