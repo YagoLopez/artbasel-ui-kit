9b7b2a3910a20b760c59f25852b92fcd570cee96
Anderson Miranda <amiranda@dminc.com>
2022-03-17 16:23:26 +0000

    Merged in feature/ABMD-1394-megamenu-dynamic-logo (pull request #399)
    fix(navigation): fixed dynamic Artbasel logo support for mobile
    
    * fix(navigation): fixed dynamic Artbasel logo support for mobile

--- packages/components/src/navigation/Megamenu/Navbar/NavbarMobile.js
+++ packages/components/src/navigation/Megamenu/Navbar/NavbarMobile.js
@@ -166,10 +166,12 @@ const NavbarMobile = ({
             />
           </Col>
           <Col className="ps-3 d-md-none">
-             { linkRenderer(menuData.logoLink, <ArtBaselLogo width={71} height={23} />) }
+              { linkRenderer(menuData.logoLink,
+                <ArtBaselLogo variant={ logoVariant } width={ 71 } height={ 23 } />) }
           </Col>
           <Col className="col-auto ps-5 pe-15 d-none d-md-block">
-             { linkRenderer(menuData.logoLink, <ArtBaselLogo width={117} height={39} />) }
+              { linkRenderer(menuData.logoLink,
+                <ArtBaselLogo variant={ logoVariant } width={ 117 } height={ 39 } />) }
           </Col>
         </Row>
       </Container>
