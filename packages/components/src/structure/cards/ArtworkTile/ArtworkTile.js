248747b844c3a618526e9d493f0d18c7756849c7
Pedro Magalhães <pmagalhaes@dminc.com>
2022-03-30 10:04:49 +0000

    Merged in fix/ABMD-1428-artwork-card (pull request #419)
    fix(artwork tile): unavailable to view state, remove curator, and support multiple tags
    
    * fix(artwork tile): unavailable to view state, remove curator, and support multiple tags
    
    * Merged main into fix/ABMD-1428-artwork-card
    
    Approved-by: Anderson Miranda

--- packages/components/src/structure/cards/ArtworkTile/ArtworkTile.js
+++ packages/components/src/structure/cards/ArtworkTile/ArtworkTile.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import { Button } from '../../../actions/Button';
 import { ButtonIcon } from '../../../actions/ButtonIcon';
+import { Icon } from '../../../utils/Icon';
 import { Tag } from '../../../feedback/Tag';
 import { Checkbox } from '../../../inputs/Checkbox';
 
@@ -38,7 +39,7 @@ const ArtworkTile = ({
   linkRenderer,
   viewAtFair,
   onlineExclusive,
-  curator,
+  unavailableToView,
   show,
   selectMode,
 }) => {
@@ -68,7 +69,8 @@ const ArtworkTile = ({
         [`layout-${imageType}`]: imageType,
         [`page-${pageType}`]: pageType,
         'inquiry-state': inquiry?.active,
-        'default-state': !selectMode?.active,
+        'default-state': !selectMode?.active && !unavailableToView?.active,
+        'unavailable-state': unavailableToView?.active,
         'select-state': selectMode?.active,
         'selected-state': isSelected,
       })}
@@ -91,27 +93,35 @@ const ArtworkTile = ({
             className="checkbox-select"
           />
         )}
-        {onlineExclusive?.active && (
-          <Tag
-            label={onlineExclusive.label}
-            onClick={onlineExclusive.onClick}
-            className="tag-exclusive"
-          />
-        )}
-        {viewAtFair?.active && (
-          <Tag
-            label={viewAtFair.label}
-            onClick={viewAtFair.onClick}
-            className="tag-viewfair"
-          />
-        )}
-        {curator?.active && (
-          <Tag
-            label={curator.label}
-            className="tag-curator"
-            onClick={curator.onClick}
-            type="curator"
-          />
+        <div className="tag-container">
+          {unavailableToView?.active && (
+            <Tag
+              label={unavailableToView.label}
+              onClick={unavailableToView.onClick}
+              className="tag-unavailableToView"
+              icon="Info"
+              iconAlign="left"
+            />
+          )}
+          {onlineExclusive?.active && (
+            <Tag
+              label={onlineExclusive.label}
+              onClick={onlineExclusive.onClick}
+              className="tag-exclusive"
+            />
+          )}
+          {viewAtFair?.active && (
+            <Tag
+              label={viewAtFair.label}
+              onClick={viewAtFair.onClick}
+              className="tag-viewfair"
+            />
+          )}
+        </div>
+        {unavailableToView?.active && (
+          <div className="overlay-eye">
+            <Icon name="eye-hide" />
+          </div>
         )}
         {inquiry?.active && (
           <Button
@@ -132,6 +142,11 @@ const ArtworkTile = ({
           <div
             className={classNames('overlay-fill dark', {
               selected: selectMode?.active && isSelected,
+            })}
+          />
+          <div
+            className={classNames('overlay-blur', {
+              light: unavailableToView?.active,
             })}
           />
         </MemoizedConditionalWrapper>
@@ -264,17 +279,17 @@ ArtworkTile.propTypes = {
     label: PropTypes.string.isRequired,
     onClick: PropTypes.func,
   }),
+  unavailableToView: PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+  }),
   viewAtFair: PropTypes.shape({
     active: PropTypes.bool.isRequired,
     label: PropTypes.string.isRequired,
     onClick: PropTypes.func,
   }),
   onlineExclusive: PropTypes.shape({
-    active: PropTypes.bool.isRequired,
-    label: PropTypes.string.isRequired,
-    onClick: PropTypes.func,
-  }),
-  curator: PropTypes.shape({
     active: PropTypes.bool.isRequired,
     label: PropTypes.string.isRequired,
     onClick: PropTypes.func,
