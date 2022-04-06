31e54e28afa800f1e3fdb69871dc5617a96b17f0
Florian <fvgarcia@dminc.com>
2022-04-06 10:56:28 +0000

    feat(divider): add  className prop in Divider component

--- packages/components/src/structure/Divider/Divider.js
+++ packages/components/src/structure/Divider/Divider.js
@@ -2,17 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const colorsList = ['mch-black', 'mch-white', 'mch-gray-100', 'mch-gray-200', 'mch-gray-300'];
+const colorsList = [
+  'mch-black',
+  'mch-white',
+  'mch-gray-100',
+  'mch-gray-200',
+  'mch-gray-300',
+];
 
-const Divider = ({ orientation, size, color }) => {
-  return (
-    orientation === 'horizontal' ? (
-        <div data-testid="mch-divider" className="divider">
-            <hr className={classNames(size, color)} />
-        </div>
-
-    ) : <div data-testid="mch-divider" className={classNames('divider vertical vr', size, color)} />
-
+const Divider = ({
+  orientation, size, color, className,
+}) => {
+  return orientation === 'horizontal' ? (
+    <div data-testid="mch-divider" className="divider">
+      <hr className={classNames(size, color, className)} />
+    </div>
+  ) : (
+    <div
+      data-testid="mch-divider"
+      className={classNames('divider vertical vr', size, color, className)}
+    />
   );
 };
 
@@ -20,12 +29,14 @@ Divider.propTypes = {
   orientation: PropTypes.oneOf(['horizontal', 'vertical']),
   size: PropTypes.oneOf(['s', 'm']),
   color: PropTypes.oneOf(colorsList).isRequired,
+  className: PropTypes.string,
 };
 
 Divider.defaultProps = {
   orientation: 'horizontal',
   size: 'm',
   color: 'mch-black',
+  className: '',
 };
 
 export default Divider;
