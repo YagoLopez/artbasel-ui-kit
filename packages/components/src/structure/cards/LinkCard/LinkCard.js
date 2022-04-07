5223a5ed2b1be524807578d615ef88da11ac5364
Fernando Lucero <flucero@dminc.com>
2022-04-07 11:16:32 +0000

    fix(linkcard): style fix, structure and transition changes

--- packages/components/src/structure/cards/LinkCard/LinkCard.js
+++ packages/components/src/structure/cards/LinkCard/LinkCard.js
@@ -2,7 +2,6 @@ import React, { useMemo } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { Icon } from '../../../utils/Icon';
-import { Container, Row, Col } from '../../Grid';
 
 const LinkCard = ({
   variant,
@@ -14,56 +13,24 @@ const LinkCard = ({
   const isSecondary = useMemo(() => variant === 'secondary', [variant]);
 
   return (
-    <Container
+    <div
       data-testid="mch-link-card"
-      fluid
-      className={classNames('link-card', { secondary: isSecondary })}
+      className={classNames('link-card d-flex justify-content-between',
+        { secondary: isSecondary })}
       onClick={onClick}
     >
-      <Row
-        gutter="g-0"
-        className={classNames('d-flex', {
-          'd-md-none': showDescription,
-          'd-md-flex': !showDescription,
-        })}
-      >
-        <Col className="headline" xs={8}>
+      <div>
+        <div className="headline">
           <span className="headline-underline">{title}</span>
-        </Col>
-        <Col className="d-flex justify-content-end">
+        </div>
+        {showDescription
+          && <p className="description mb-0 mt-3">{description}</p>
+        }
+        <div className="d-flex flex-column justify-content-end icon-wrapper">
           <Icon name="chevron-right" size={24} />
-        </Col>
-      </Row>
-
-      <Row
-        gutter="g-0"
-        className={classNames('headline', 'd-none', {
-          'd-md-flex': showDescription,
-          'd-md-none': !showDescription,
-        })}
-      >
-        <span className="headline-underline">{title}</span>
-      </Row>
-      <Row
-        gutter="g-0"
-        className={classNames('d-none', 'd-md-flex', {
-          'd-md-none': !showDescription,
-        })}
-      >
-        <Col className="description" xs={8}>
-          {description}
-        </Col>
-        <Col
-          className={classNames(
-            'd-flex',
-            'justify-content-end',
-            'align-items-center',
-          )}
-        >
-          <Icon name="chevron-right" size={24} />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 
