eaba83b66fe36020bd8d69e3d41477ccfd7dab27
Fernando Lucero <flucero@dminc.com>
2022-05-19 16:13:37 +0000

    fix(megamenu): increase the clickable area for profile flyout link

--- packages/components/src/navigation/Megamenu/Navbar/ProfileFlyout.js
+++ packages/components/src/navigation/Megamenu/Navbar/ProfileFlyout.js
@@ -82,7 +82,7 @@ const ProfileFlyout = ({
                 {o.label}
               </li>
           ) : (
-              <li className="item-menu-label item-menu-link" onClick={() => setIsVisible(null)} key={o.label}>
+              <li className="item-menu-label item-menu-link profile-link" onClick={() => setIsVisible(null)} key={o.label}>
                 { linkRenderer(o.link, o.label, o.target) }
               </li>
           )))}
