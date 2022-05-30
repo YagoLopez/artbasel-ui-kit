0962247cbabdb2a0ed82abead21d2721c59b085c
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-05-30 07:25:26 +0000

    feat(mini card): unavailable image state added

--- packages/components/src/structure/cards/ArtistGalleryMiniCard/ArtistGalleryMiniCard.js
+++ packages/components/src/structure/cards/ArtistGalleryMiniCard/ArtistGalleryMiniCard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import ButtonIcon from '../../../actions/ButtonIcon/ButtonIcon';
 import Checkbox from '../../../inputs/Checkbox/Checkbox';
 import Tag from '../../../feedback/Tag/Tag';
+import { Icon } from '../../../utils/Icon';
 
 const ConditionalWrapper = ({
   linkRenderer, condition, link, children,
@@ -20,6 +21,7 @@ const ArtistGalleryMiniCard = ({
   selectMode, title, variant, tagCities,
   className, onClickTag, image,
   collection, linkRenderer, collectionLink,
+  unavailable,
 }) => {
   if (!title || !image) return null;
 
@@ -85,7 +87,8 @@ const ArtistGalleryMiniCard = ({
             <h5 title={title} className={classNames('me-10 position-relative', {
               'text-white mini-card-title-artist': variant === 'artist',
               'mini-card-title opacity-100': variant === 'gallery',
-              'mini-card-title-artist': !selectMode.active,
+              'mini-card-title-artist opacity-title': !selectMode.active,
+              'mini-card-title-artist opacity-100': selectMode.active,
             })}>{title}</h5>
           </MemoizedConditionalWrapper>
         </div>
@@ -100,8 +103,32 @@ const ArtistGalleryMiniCard = ({
           )
         }
       </header>
-      <main>
-        <img src={image} alt="mini card image" className="w-100"/>
+      <main className={classNames({
+        'position-relative': unavailable,
+      })}>
+        <img src={image} alt="mini card image" className={classNames('w-100', {
+          'blur-image': unavailable,
+        })}/>
+        {
+          unavailable && (
+            <>
+              <div className='info-text position-absolute d-flex'>
+                <Icon
+                  name="Info"
+                  size={18}
+                />
+                <p className='text-label-small my-0'>Unavailable to view</p>
+              </div>
+              <div className='position-absolute h-100 w-100 top-0 unavailable-image d-flex justify-content-center align-items-center p-5'>
+                <div className='eye-hide-box p-4 rounded-circle'>
+                  <Icon
+                    name="eye-hide"
+                  />
+                </div>
+              </div>
+            </>
+          )
+        }
       </main>
       <footer className='position-absolute bottom-0 start-0 ms-5 my-5'>
         {
@@ -139,6 +166,7 @@ ArtistGalleryMiniCard.propTypes = {
   onClickTag: PropTypes.func,
   linkRenderer: PropTypes.func,
   collectionLink: PropTypes.string,
+  unavailable: PropTypes.bool,
 };
 
 ArtistGalleryMiniCard.defaultProps = {
@@ -153,6 +181,7 @@ ArtistGalleryMiniCard.defaultProps = {
     active: true,
     onClick: () => { },
   },
+  unavailable: false,
 };
 
 export default ArtistGalleryMiniCard;
